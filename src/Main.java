1987d1ebe62f8b7d01682fd70b7c742a727555f2
Vladlen Yamalov <v011088@yandex.ru>
2022-07-23 15:47:11 +0700

    Изменен тип на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales={1000,2000,1500};
+        long[] sales={1000,2000,1500};
         SalesManager salesManager=new SalesManager(sales);
         System.out.println(salesManager.max());
 
